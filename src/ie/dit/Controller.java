a0ffd2ca2e477ec7695714dc5ee2f3bf476cb60c
Emmet Doyle <Emmet Doyle>
2016-11-01 17:18:10 +0000

    initial commit

---
+++ src/ie/dit/Controller.java
@@ -0,0 +1,12 @@
+package ie.dit;
+
+/**
+ * Created by Emmet on 01/11/2016.
+ */
+public class Controller {
+
+    public static void main(String args[]){
+        System.out.println("Hello, World!");
+    }
+
+}
